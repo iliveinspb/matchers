cc2ab2ae90c226afb081548faa0c306ad4508040
Anna Baraeva <pfeffa.hain@gmail.com>
2026-02-27 18:07:12 +0300

    Add Sort.js and test

---
+++ src/js/__tests__/sort.test.js
@@ -0,0 +1,22 @@
+import sortHeroes from "../sort";
+
+test("sorts heroes from healthiest to weakest", () => {
+  const heroes = [
+    { name: "мечник", health: 10 },
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+  ];
+
+  const sorted = sortHeroes(heroes);
+
+  expect(sorted).toEqual([
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+    { name: "мечник", health: 10 },
+  ]);
+});
+
+test("array with one hero returns same array", () => {
+  const hero = [{ name: "маг", health: 100 }];
+  expect(sortHeroes(hero)).toEqual(hero);
+});
