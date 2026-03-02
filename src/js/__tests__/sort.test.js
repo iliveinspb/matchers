d0519aa9f7ea470776795fe96164d7b2b3ca3e3b
Anna Baraeva <pfeffa.hain@gmail.com>
2026-03-02 18:58:38 +0300

    configure eslint

--- src/js/__tests__/sort.test.js
+++ src/js/__tests__/sort.test.js
@@ -1,22 +1,22 @@
-import sortHeroes from "../sort";
+import sortHeroes from '../sort';
 
-test("sorts heroes from healthiest to weakest", () => {
+test('sorts heroes from healthiest to weakest', () => {
   const heroes = [
-    { name: "мечник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
 
   const sorted = sortHeroes(heroes);
 
   expect(sorted).toEqual([
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
 });
 
-test("array with one hero returns same array", () => {
-  const hero = [{ name: "маг", health: 100 }];
+test('array with one hero returns same array', () => {
+  const hero = [{ name: 'маг', health: 100 }];
   expect(sortHeroes(hero)).toEqual(hero);
 });
